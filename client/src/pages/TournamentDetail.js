2ec5102a55847b67ceda638dfa75942a4f3a1f9e
Justin Rodriguez <justin.r@me.com>
2024-07-09 21:41:42 -0400

    routes.js setup

---
+++ client/src/pages/TournamentDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TournamentDetail() {
+  return (
+    <div>TournamentDetail</div>
+  )
+}
+
+export default TournamentDetail
---
+++ client/src/routes.js
@@ -0,0 +1,35 @@
+import App from "./components/App";
+import Home from "./pages/Home";
+import Players from "./pages/Players";
+import Cards from "./pages/Cards";
+import Tournaments from "./pages/Tournaments";
+import TournamentDetail from "./pages/TournamentDetail";
+
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: '/players',
+                element: <Players />
+            },
+            {
+                path: '/cards',
+                element: <Cards />
+            },
+            {
+                path: '/tournaments',
+                element: <Tournaments />
+            },
+            {
+                path: `/tournaments/${id}`,
+                element: <TournamentDetail />
+            }
+        ]
+    }
+]
