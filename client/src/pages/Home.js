1e0bedf0ddcafb32a9389e7dcb028ce53b6b5132
Justin Rodriguez <justin.r@me.com>
2024-07-15 15:27:41 -0400

    home page layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,47 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
-import { Box, Typography } from '@mui/material'
-import { useOutletContext } from 'react-router-dom'
-// Something like a carousel
+import { Box, Typography, Card, CardContent, CardActionArea, Grid } from '@mui/material'
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 
 function Home() {
-
-    const {players} = useOutletContext()
-    const images = players.ownerships ? players[35].ownerships.map(c => c.cards.image_url) : []
-    
-    console.log(images)
 
     return (
         <>
             <NavBar />
             <Box maxWidth='100%' sx={{m: 'auto'}} >
-                <Typography textAlign={'center'} variant='h2'>Welcome to MTG Collections</Typography>
+                <Typography textAlign={'center'} variant='h2' sx={{mt: '50px'}}>Welcome to MTG Collections</Typography>
+                <Grid2 container spacing={2} sx={{mt: '50px', ml: '50px', mr: '50px'}}>
+                    <Grid2 xs={6}>
+                        <Card>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography textAlign={'center'} variant='h4'>Players</Typography>
+                                    <Typography textAlign={'center'} variant='body1' color='text.secondary'>View a list of players, their card collection and any tournaments they are registered in.</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid2>
+                    <Grid2 xs={6}>
+                        <Card>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography textAlign={'center'} variant='h4'>Cards</Typography>
+                                    <Typography textAlign={'center'} variant='body1' color='text.secondary'>Search for cards by name, rarity and more.</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid2>
+                    <Grid2 xs={12}>
+                        <Card>
+                            <CardActionArea>
+                                <CardContent>
+                                    <Typography textAlign={'center'} variant='h4'>Tournaments</Typography>
+                                    <Typography textAlign={'center'} variant='body1' color='text.secondary'>View a list of tournaments and their registrants.</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid2>
+                </Grid2>
             </Box>
         </>
     )
