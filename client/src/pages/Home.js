818e4c7efd5d9f61c32ddab26bd49b07ecd91ce8
Justin Rodriguez <justin.r@me.com>
2024-07-15 15:32:45 -0400

    home cards routing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Link } from 'react-router-dom'
 import { Box, Typography, Card, CardContent, CardActionArea, Grid } from '@mui/material'
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
 
@@ -13,7 +14,7 @@ function Home() {
                 <Grid2 container spacing={2} sx={{mt: '50px', ml: '50px', mr: '50px'}}>
                     <Grid2 xs={6}>
                         <Card>
-                            <CardActionArea>
+                            <CardActionArea component={Link} to='/players'>
                                 <CardContent>
                                     <Typography textAlign={'center'} variant='h4'>Players</Typography>
                                     <Typography textAlign={'center'} variant='body1' color='text.secondary'>View a list of players, their card collection and any tournaments they are registered in.</Typography>
@@ -23,7 +24,7 @@ function Home() {
                     </Grid2>
                     <Grid2 xs={6}>
                         <Card>
-                            <CardActionArea>
+                            <CardActionArea component={Link} to='/cards'>
                                 <CardContent>
                                     <Typography textAlign={'center'} variant='h4'>Cards</Typography>
                                     <Typography textAlign={'center'} variant='body1' color='text.secondary'>Search for cards by name, rarity and more.</Typography>
@@ -33,7 +34,7 @@ function Home() {
                     </Grid2>
                     <Grid2 xs={12}>
                         <Card>
-                            <CardActionArea>
+                            <CardActionArea component={Link} to='/tournaments'>
                                 <CardContent>
                                     <Typography textAlign={'center'} variant='h4'>Tournaments</Typography>
                                     <Typography textAlign={'center'} variant='body1' color='text.secondary'>View a list of tournaments and their registrants.</Typography>
