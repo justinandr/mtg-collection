72498f9780925253e7357e84e48226726a03936a
Justin Rodriguez <justin.r@me.com>
2024-07-17 00:03:21 -0400

    build card search form

--- client/src/components/CardSearchForm.js
+++ client/src/components/CardSearchForm.js
@@ -5,42 +5,102 @@ import { Form } from 'react-router-dom'
 
 function CardSearchForm() {
 
+    const [name, setName] = useState('')
     const [rarity, setRarity] = useState('')
+    const [type, setType] = useState('')
+    const [rarityOpen, setRarityOpen] = useState(false)
+    const [typeOpen, setTypeOpen] = useState(false)
+    const types = ["Artifact", "Battle", "Conspiracy" ,"Creature" ,"Dragon" ,
+        "Elemental", "Enchantment", "Goblin", "Hero", "instant", "Instant", 
+        "Jaguar", "Kindred", "Knights", "Land", "Legend", "Phenomenon", "Plane", 
+        "Planeswalker", "Scheme", "Sorcery", "Stickers", "Summon", "Tribal", 
+        "Universewalker", "Vanguard", "Wolf"]
+    const rarities = ['Common', 'Uncommon', 'Rare', 'Mythic']
 
-    const items = ['1', '2', '3', '4', '5', '6']
+    function handleRarityClose(){
+        setRarityOpen(false)
+    }
 
-    function handleChange(e){
-        setRarity(e.target.value)
+    function handleRarityOpen(){
+        setRarityOpen(true)
+    }
+
+    function handleTypeClose(){
+        setTypeOpen(false)
+    }
+
+    function handleTypeOpen(){
+        setTypeOpen(true)
     }
 
     return (
-
-        <Box sx={{minWidth: 120, m: 'auto'}}>
-            <FormControl fullWidth>
-                <TextField id='card-name' label='Card Name'></TextField>
-            </FormControl>
-            {/* <Grid2 container >
-                <Grid2 xs={12}>
-                    <Typography variant='h2' textAlign={'center'} gutterBottom>Card Search</Typography>
+        <Box
+        sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <Typography variant='h4'>Card Search</Typography>
+            <Box
+            component='form'
+            sx={{mt: '20px'}}
+            >
+                <Grid2 container spacing={2}>
+                    <Grid2 xs={12} sm={6}>
+                        <TextField
+                        sx={{mt: '25px'}}
+                        fullWidth
+                        label='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                    </Grid2>
+                    <Grid2 xs={12} sm={6}>
+                        <InputLabel id='rarity'>Rarity</InputLabel>
+                        <Select
+                            labelId='rarity'
+                            fullWidth
+                            label='Rarity'
+                            open={rarityOpen}
+                            onClose={handleRarityClose}
+                            onOpen={handleRarityOpen}
+                            value={rarity}
+                            onChange={(e) => setRarity(e.target.value)}
+                        >
+                            {rarities.map(rarity => {
+                                return(
+                                    <MenuItem key={rarity} value={rarity}>{rarity}</MenuItem>
+                                )
+                            })}
+                        </Select>
+                    </Grid2>
+                    <Grid2 xs={12}>
+                        <InputLabel id='type'>Type</InputLabel>
+                        <Select
+                            fullWidth
+                            labelId='type'
+                            label='Type'
+                            open={typeOpen}
+                            onClose={handleTypeClose}
+                            onOpen={handleTypeOpen}
+                            value={type}
+                            onChange={(e) => setType(e.target.value)}
+                        >
+                            {types.map(type => {
+                                return(
+                                    <MenuItem key={type} value={type}>{type}</MenuItem>
+                                )
+                            })}
+                        </Select>
+                    <Grid2 xs={12} sx={{mt: '25px'}}>
+                        <Button fullWidth variant='contained'>Submit</Button>
+                    </Grid2>
+                    </Grid2>
                 </Grid2>
-                <Grid2 xs={4}>
-                    <TextField label='Card Name'/>
-                </Grid2>
-                <Grid2 xs={4}>
-                    <Select
-                        id='rarity'
-                        value={rarity}
-                        label="Rarity"
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={1}>Rarity 1</MenuItem>
-                    </Select>
-                </Grid2>
-                <Grid2 xs={4}>
-                    <Button>Button</Button>
-                </Grid2>
-            </Grid2> */}
-        </Box>
+            </Box>
+        </Box> 
     )
 }
 
