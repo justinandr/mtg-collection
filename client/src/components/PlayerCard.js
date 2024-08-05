ab8b0195913c3b505bda501bfe8269ea1449b304
Justin Rodriguez <justin.r@me.com>
2024-08-04 21:14:08 -0400

    make player cards clickable

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardActions, CardContent, Button, Typography, Box } from '@mui/material'
+import { Card, CardActions, CardContent, Button, Typography, Box, CardActionArea } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function PlayerCard({player, registrations}) {
@@ -7,13 +7,12 @@ function PlayerCard({player, registrations}) {
     return (
         <Box sx={{minWidth: 275}}>
             <Card variant='outlined'>
+                <CardActionArea component={Link} to={`/players/${player.id}`}>
                     <CardContent>
                         <Typography variant='h4' >{player.name}</Typography>
                         <Typography variant='h6'>Tournaments Played: {player.tournaments_played}</Typography>
                     </CardContent>
-                    <CardActions>
-                        <Button component={Link} to={`/players/${player.id}`}>View More</Button>
-                    </CardActions>
+                </CardActionArea>
             </Card>
         </Box>
     )
